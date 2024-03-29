1dc904f0a4a084b9b2a1d3103f73380eb005015a
minsung-kwon <87001868+minsung-kwon@users.noreply.github.com>
2024-03-29 09:40:00 +0900

    Update week1_1.cpp

--- week1_1.cpp
+++ week1_1.cpp
@@ -4,10 +4,17 @@
 
 using namespace std;
 
+/**
+*애너그램인지 아닌지 판별하는 함수
+*애너그램이면 true, 아니면 false반환하는 함수
+*/
 bool JudgeAnagram(string str1, string str2) {
     if (str1.length() != str2.length())
         return false;
     map<char, int> dict1; map<char, int> dict2;
+    //c++ STL의 map객체
+    //한번 들어간 char는 다시 들어가지 않고, 조정 불가능하다. int는 변환이 가능하다.
+    //char을 자동으로 정렬해준다.
     for (int i = 0; i < str1.length(); i++) {
         if (dict1.find(str1[i]) != dict1.end())
             dict1[str1[i]] += 1;
@@ -33,6 +40,10 @@ int main()
         cin >> N;
     string str_left[100];
     string str_right[100];
+    //string은 c++표준 라이브러리의 객체이다. 문자열의 입력/출력, char배열과 같이 각 인덱스에도 접근이 가능하다. 정확히는 객체이지만 그냥 자료형으로 생각하자.
+    //char배열의 경우 ==과 같은 연산자를 사용한 비교가 어렵고, strcmp()와 같은 함수를 사용하거나 반복문을 이용해서 각 인덱스별로 비교해야 한다. 
+    //하지만 string객체를 이용할 경우 일반적인 연산자만을 이용하여 모든 작업이 가능하다.
+    //좀 기능 적은 python string이라고 생각하면 된다.
     for (int i = 0; i < N; i++)
         cin >> str_left[i] >> str_right[i];
     for (int i = 0; i < N; i++) {
